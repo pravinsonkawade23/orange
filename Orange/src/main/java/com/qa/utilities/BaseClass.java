f748ac012684c2198e86d1ee015e52142c94c75f
pravinsonkawade23 <pravinsonkawade23@gmail.com>
2024-07-23 15:49:22 +0530

    first

---
+++ Orange/src/main/java/com/qa/utilities/BaseClass.java
@@ -0,0 +1,23 @@
+package com.qa.utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+public class BaseClass {
+	
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void openBrowser() {
+		
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		
+		
+		
+	}
+
+}
---
+++ Orange/src/main/java/com/qa/utilities/FunctionClass.java
@@ -0,0 +1,10 @@
+package com.qa.utilities;
+
+public class FunctionClass {
+	
+	public static void getDataFromPF(String key) {
+		
+		
+	}
+
+}
