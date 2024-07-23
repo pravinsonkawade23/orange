cc2b6371dfc661282941fd45a89e9c1dbee69c89
pravinsonkawade23 <pravinsonkawade23@gmail.com>
2024-07-23 16:10:21 +0530

    third

--- Orange/src/main/java/com/qa/utilities/BaseClass.java
+++ Orange/src/main/java/com/qa/utilities/BaseClass.java
@@ -1,5 +1,7 @@
 package com.qa.utilities;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
